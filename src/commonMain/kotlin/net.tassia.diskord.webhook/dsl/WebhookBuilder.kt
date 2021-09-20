52efd7af4e7af8b69d59da4007f8f0178636500e
Tassilo <tassia710@gmail.com>
2021-09-20 22:46:49 +0200

    Add URL parsing

--- src/commonMain/kotlin/net.tassia.diskord.webhook/dsl/WebhookBuilder.kt
+++ src/commonMain/kotlin/net.tassia.diskord.webhook/dsl/WebhookBuilder.kt
@@ -7,10 +7,16 @@ class WebhookBuilder {
 
 	var id: Long? = null
 	var token: String? = null
+	var server: String? = null
 	var client: HttpClient? = null
 
 	fun url(url: String) {
-		TODO()
+		val regex = Regex("([A-z0-9/.]+)/([0-9]+)/([A-z0-9_-]+)")
+		val match = regex.matchEntire(url) ?: throw IllegalArgumentException("Illegal webhook URL")
+
+		this.id = match.groupValues[2].toLong()
+		this.token = match.groupValues[3]
+		this.server = match.groupValues[1]
 	}
 
 
@@ -19,6 +25,7 @@ class WebhookBuilder {
 		return WebhookContext(
 			webhookID = id ?: throw IllegalStateException("The ID of the webhook was not specified."),
 			webhookToken = token ?: throw IllegalStateException("The token of the webhook was not specified."),
+			server = server ?: throw IllegalStateException("No server was specified."),
 			client = client ?: HttpClient(),
 		)
 	}
